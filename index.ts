0d601631cc86e0c573188d34f6919dc517ce5e6b
saptarshi dutta <saptarshidutta599@gmail.com>
2023-09-17 18:46:42 +0530

    data

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ import axios from "axios"
 
 require('dotenv').config();
 const express = require('express');
+const { OpenAI } = require("langchain/llms/openai");
+const { TextLoader } = require("langchain/document_loaders/fs/text");
+
 const app=express();
 app.use(cors());
 
@@ -12,11 +15,25 @@ const t=initTRPC.create();
 export const router =t.router;
 export const publicProcedure=t.procedure;
 
+const llm = new OpenAI({
+    openAIApiKey: "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",
+    temperature: 0.9,
+  });
+
+
+  const loader = new TextLoader("src/document_loaders/example_data/example.txt");
+  
+  
+  
 
 export const appRouter=router({
     get:publicProcedure.query(async()=>{
         const jokes=(await axios.get('https://api.chucknorris.io/jokes/random')).data
-        return jokes.value
+        const res=await llm.call("What would be a good company name for a company that makes colorful socks?");
+        
+        const docs = await loader.load();
+
+        return res;
     })
 })
 
